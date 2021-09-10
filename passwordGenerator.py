0cc1c664af39e88b550d4fd1d3310b3ea4df9966
ndwrobotics <22948910+ndwrobotics@users.noreply.github.com>
2021-09-09 22:50:37 -0400

    Finally converting from archaic 2.7.5

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,14 +5,14 @@ random.seed()
 
 while True:
     try:
-        length = int(raw_input("Length? "))
+        length = int(input("Length? "))
         break
     except ValueError:
-        print "Enter a number."
+        print("Enter a number.")
 
 password = ''
 characters = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*?"
 for i in range(length):
 	password += characters[random.randint(0, len(characters)-1)]
 
-print password
+print(password)
